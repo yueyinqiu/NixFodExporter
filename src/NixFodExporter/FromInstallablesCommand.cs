d2b01c61963cc89224f6f0c5a0eadeb490a64a1f
yueyinqiu <yueyinqiu@outlook.com>
2026-08-11 16:11:06 +0800

    e

--- src/NixFodExporter/FromInstallablesCommand.cs
+++ src/NixFodExporter/FromInstallablesCommand.cs
@@ -18,8 +18,12 @@ public partial class FromInstallablesCommand : ICommand
     public async ValueTask ExecuteAsync(IConsole console)
     {
         var cancellationToken = console.RegisterCancellationHandler();
+
         var command = Cli.Wrap("nix").WithArguments(["derivation", "show", "-r", .. Installables]);
+        Console.WriteLine(command);
         var result = await command.ExecuteBufferedAsync(cancellationToken);
+        Console.WriteLine(result.StandardOutput);
+        
         await new FromDerivationsCommand()
         {
             Output = this.Output,
